9fe50ac16965b5a79e164571b848f36f2c75b671
Oliver Han <hanmayingze@gmail.com>
2019-06-03 23:59:27 -0400

    Initial Commit

---
+++ Landmark-SwiftUI Practice/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmark-SwiftUI Practice
+//
+//  Created by Oliver Hann on 6/3/19.
+//  Copyright © 2019 Oliver Hann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
