31fb01fdc2575dfed47ea2d1ad738707bc939593
Oliver Han <hanmayingze@gmail.com>
2019-06-04 18:42:24 -0700

    section 2: customize the text view

--- Landmark-SwiftUI Practice/ContentView.swift
+++ Landmark-SwiftUI Practice/ContentView.swift
@@ -9,8 +9,13 @@
 import SwiftUI
 
 struct ContentView : View {
+    
+    // MARK: - UI
+    // `some` represents [opaque result types](https://github.com/apple/swift-evolution/blob/master/proposals/0244-opaque-result-types.md)
     var body: some View {
-        Text("Hello Oliver!")
+        Text("Turtle Rock")
+            .font(.title) // `.font()` is a modifier
+            .color(.green) // `.color()` is a modifier
     }
 }
 
